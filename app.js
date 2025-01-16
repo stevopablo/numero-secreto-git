7df646f090539c60db321561447a48527a173400
stevopablo <stevaopabllo@gmai.com>
2025-01-16 13:45:35 -0300

    Revert "algumas mudanças"
    This reverts commit ddb8a0f65edf809541a94e1cb7cf6360cdbb7801.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 let listaDeNumerosSorteados = [];
 
 // Set the limit for the random number
-let numeroLimite = 100;
+let numeroLimite = 10;
 
 // Generate a random secret number
 let numeroSecreto = gerarNumeroAleatorio();
